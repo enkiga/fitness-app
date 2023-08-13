20749fc56660f2f6360a29250540d31a9a5c4746
enkiga <eenkiga@gmail.com>
2023-08-13 22:35:39 +0300

    ContactUsPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import OurClasses from "@/scenes/our-classes";
 import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contact-us";
 import {useEffect, useState} from "react";
 import {SelectedPage} from "@/shared/types";
 
@@ -33,6 +34,7 @@ function App() {
             <Home setSelectedPage={setSelectedPage}/>
             <Benefits setSelectedPage={setSelectedPage}/>
             <OurClasses setSelectedPage={setSelectedPage}/>
+            <ContactUs setSelectedPage={setSelectedPage}/>
         </div>
     )
 }
